9f3cb25aa267d07a0313cc95022c00c4e2c4429a
boomboomgzy <3228352301@qq.com>
2023-09-20 13:08:36 +0000

    rectangle::to_string done

--- src/rectangle.cpp
+++ src/rectangle.cpp
@@ -1,12 +1,15 @@
 #include "../include/rectangle.h"
 
 #include<iostream>
+#include<sstream>
 
 rectangle::rectangle(place& x,place& y,distance& w,distance& h,const std::string& id):_x(x),_y(y),_w(w),_h(h),_id(id){}
 
 //使用sstream来实现
 std::string& rectangle::to_string() const{
-    std::string s="";
+    std::stringstream sstream;
+    sstream<<"("<<_x<<","<<_y<<") , "<<"w="<<_w<<", h="<<_h<<" .";
+    std::string s=sstream.str();
     return s;
 }
 
